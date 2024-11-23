7dde268daea8cfd43e2c1e04a9b5e3088af09911
witojs <witoirawan.js@gmail.com>
2024-11-23 14:13:02 +0700

    create profile screen

--- app/src/main/java/com/example/nextstep/presentation/MainActivity.kt
+++ app/src/main/java/com/example/nextstep/presentation/MainActivity.kt
@@ -1,21 +1,27 @@
 package com.example.nextstep.presentation
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nextstep.R
+import com.example.nextstep.databinding.ActivityMainBinding
+import com.example.nextstep.presentation.profile.ProfileActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvHello.setOnClickListener {
+            val intent = Intent(this, ProfileActivity::class.java)
+            startActivity(intent)
         }
     }
 }
